51320f09e37c22e258b9f61394edfb4475cf5989
simiyu <simiyu96daniel@gmail.com>
2022-09-10 10:31:31 +0300

    Added Rest ApI testing work

---
+++ udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    "password": "Alx-T-2022",
+    "database": "database1",
+    "host": "database-1.ckxmddlexkxt.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "database1-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "database1",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
